b45647edfec3e469f47d861fa11cdf62464bd445
bcarnevali <bcarnevali@correo.unimet.edu.ve>
2025-11-14 14:22:28 -0400

    Implementacion completa de clase ListaEnlazada y NodoLista

--- src/proyecto/pkg2/edd/ListaEnlazada.java
+++ src/proyecto/pkg2/edd/ListaEnlazada.java
@@ -9,24 +9,86 @@ package proyecto.pkg2.edd;
  * @author bettinacarnevali
  */
 public class ListaEnlazada {
-    private NodoLista head;
-    private NodoLista tail;
+    private NodoLista cabeza;
     private int tamano;
 
     public ListaEnlazada() {
-        this.head = null;
-        this.tail = null;
+        this.cabeza = null;
         this.tamano = 0;
     }
-    
-    //Metodos Escenciales
-    
-    public boolean esVacia(){
-        return head == null;
+
+    public boolean estaVacia() {
+        return cabeza == null;
     }
-    
-    public void agregar(Object dato){
-        NodoLista nuevoNodo = new NodoLista(dato);
+
+    public void agregar(Object dato) {
+        NodoLista nuevoNodo = new NodoLista();
+        nuevoNodo.setDato(dato);
+
+        if (this.estaVacia()) {
+            this.cabeza = nuevoNodo;
+        } else {
+            NodoLista actual = this.cabeza;
+            while (actual.getSig() != null) {
+                actual = actual.getSig();
+            }
+            actual.setSig(nuevoNodo);
+        }
+        this.tamano++;
+    }
+
+    public void recorrer() {
+        if (this.estaVacia()) {
+            System.out.println("La lista está vacía.");
+            return;
+        }
+
+        NodoLista actual = this.cabeza;
+        System.out.print("Lista Enlazada: ");
         
+        while (actual != null) {
+            System.out.print(actual.getDato() + " -> ");
+            actual = actual.getSig();
+        }
+        System.out.println("NULL");
+    }
+
+    public boolean buscar(Object datoBuscado) {
+        NodoLista actual = this.cabeza;
+        
+        while (actual != null) {
+            if (actual.getDato().equals(datoBuscado)) {
+                return true;
+            }
+            actual = actual.getSig();
+        }
+        return false;
+    }
+
+    public boolean borrar(Object datoAEliminar) {
+        if (this.estaVacia()) {
+            return false;
+        }
+
+        if (this.cabeza.getDato().equals(datoAEliminar)) {
+            this.cabeza = this.cabeza.getSig();
+            this.tamano--;
+            return true;
+        }
+
+        NodoLista actual = this.cabeza;
+        
+        while (actual.getSig() != null && !actual.getSig().getDato().equals(datoAEliminar)) {
+            actual = actual.getSig();
+        }
+        
+        if (actual.getSig() != null) {
+            NodoLista nodoAEliminar = actual.getSig();
+            actual.setSig(nodoAEliminar.getSig());
+            this.tamano--;
+            return true;
+        }
+
+        return false;
     }
 }
